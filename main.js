a97540d3839e043a171ff308e7f4f635d48d794a
Pablo Jurado <pjurado85@gmail.com>
2017-04-02 11:14:59 -0500

    add button and function to toggle class hide

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 // GO!
-
+// btn hide
+document.querySelector('#hide-nav button').addEventListener('click', function () {
+  var menu = document.querySelector('#hide-nav .nav-menu')
+  if (this.textContent === 'Show nav') {
+    this.textContent = 'Hide nav'
+  } else {
+    this.textContent = 'Show nav'
+  }
+  menu.classList.toggle('nav-menu-hidden')
+})
