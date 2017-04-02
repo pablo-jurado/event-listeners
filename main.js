afaa1e328d614399e3e6badd64b6a2d8a235f339
Pablo Jurado <pjurado85@gmail.com>
2017-04-02 12:18:15 -0500

    add remove btn to list element

--- main.js
+++ main.js
@@ -24,3 +24,26 @@ document.querySelector('#add-guest input').addEventListener('keypress', function
     }
   }
 })
+
+// task #3
+document.querySelector('#add-guest-bonus input').addEventListener('keypress', function (e) {
+  if (e.keyCode === 13) {
+    if (this.value !== '') {
+      // create btn
+      var btn = document.createElement('button')
+      btn.innerHTML = 'X'
+      btn.addEventListener('click', function () {
+        this.parentElement.remove()
+      })
+      // create li element
+      var liElement = document.createElement('li')
+      liElement.classList.add('guest')
+      liElement.textContent = this.value
+      liElement.appendChild(btn)
+      // get ul from DOM
+      var ulElement = document.querySelector('#add-guest-bonus .guest-list')
+      ulElement.appendChild(liElement)
+      this.value = ''
+    }
+  }
+})
